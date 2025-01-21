b3556479527f0d22178b95f7aad88d35410a7ec0
Alexander Koc <alexander.koc@skane.se>
2025-01-21 14:37:51 +0100

    magicmock

--- 04-mocking-primer/test_monkeypatch.py
+++ 04-mocking-primer/test_monkeypatch.py
@@ -1,21 +1,59 @@
 #!/usr/bin/env python3
 
+import pytest
+import logging
 import time
+
+"""
+Simple example using magickmock to bypass time-consuming function in tests
+"""
 
 
 class ThingieThatDoesThing:
     def __init__(self) -> None:
         pass
 
-    def do_the_thing(self):
-        result = self._time_consuming_subthing()
-        if result:
-            print("Did the thing!")
+    def do_thing_with_some_scout_case(self):
+        """
+        Fetch and process a case.
+        """
+        result = self._fetch_scout_case()
+        result["thing_done"] = True
+        return result
 
-    def _time_consuming_subthing(self):
-        time.sleep(secs=10)
-        return True
+    def _fetch_scout_case(self) -> dict:
+        """
+        Slow network call. We don't want this to run in our tests.
+        """
+        logging.info("Fetching case...")
+        time.sleep(15)
+
+        case_obj = {"case_id": "foo", "status": "archived"}
+
+        return case_obj
 
 
-def test_do_the_thing():
+@pytest.fixture()
+def thingie():
+    return ThingieThatDoesThing()
+
+
+@pytest.fixture()
+def test_case():
+    case_obj = {"case_id": "bar", "status": "active", "thing_done": True}
+    return case_obj
+
+
+@pytest.fixture()
+def patched_thingie():
+    ...
+
+
+def test_do_the_thing(patched_thingie):
+    result = patched_thingie.do_thing_with_some_scout_case()
+    assert result["case_id"] == "foo"
+    assert result["thing_done"]
+
+
+def test_raise_exception_if_thing_not_done():
     ...
