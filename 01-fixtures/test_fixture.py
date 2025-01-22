3b966f6e56715e9fa309659e76f2857a13b3ade6
Alexander Koc <alexander.koc@skane.se>
2025-01-22 07:46:47 +0100

    final edits to fixture demo

--- 01-fixtures/test_fixture.py
+++ 01-fixtures/test_fixture.py
@@ -1,6 +1,9 @@
-# 1. Show how to make a fixture
-# 2. Show how to pass a fixture to a test
-# 3. Move test_data fixture and move to conftest.py
+"""
+ 1. Show how to make a fixture:
+ 2. Show how to pass a fixture to a test
+ 3. Move test_data fixtures and move to conftest.py
+ (4. Use requests fixture to fetch multiple test cases?)
+"""
 
 
 def check_coverage(test_data: dict, min_coverage: float):
@@ -15,15 +18,15 @@ def check_insert_length(test_data: dict, min_insert_length: int):
     return test_data["insert_size"] >= min_insert_length
 
 
-def test_check_coverage():
-    test_data = {
-        "sample_id": "test_sample",
-        "sample_type": "tumor",
-        "coverage": 45,
-        "contamination": 0.02,
-        "insert_size": 150,
-    }
-    ...
+# def test_check_coverage():
+#     test_data = {
+#         "sample_id": "test_sample",
+#         "sample_type": "tumor",
+#         "coverage": 45,
+#         "contamination": 0.02,
+#         "insert_size": 150,
+#     }
+#     ...
 
 
 # def test_check_coverage(): ...
