2ae1149f19fd24b405b71d155959c5a179759d14
Alexander Koc <alexander.koc@skane.se>
2025-02-07 11:49:23 +0100

    set up test class for mongomocking

--- day_02/01-mocking-primer/test_mongomock.py
+++ day_02/01-mocking-primer/test_mongomock.py
@@ -1,26 +1,52 @@
 #!/usr/bin/env python3
 import pymongo
+import pytest
+import mongomock
+import logging
 
 
-class MongoDB:
-    def __init__(self, mongo_uri: str = "localhost:27017"):
-        self.cx = MongoDB._get_mongoclient(mongo_uri)
+class SampleHandler:
+    """
+    Fetches samples from mongo.foo.bar
+    """
+
+    DB_NAME = "foo"
+    COLLECTION_NAME = "bar"
+
+    def __init__(self):
+        self.client = None
+
+    def initialize_from_mongo_uri(self, mongo_uri: str):
+        """
+        Initialize SampleHandler / MongoDB connection
+        """
+        self.client = self._get_mongoclient(mongo_uri)
+        logging.debug("initialized client: %s", type(self.client))
+        self.db = self.client[self.DB_NAME]
+        self.cx = self.db[self.COLLECTION_NAME]
+
+    def get_sample(self, sample_id: str):
+        """
+        Fetch a sample by id from mongo
+        """
+        query = {"id": sample_id}
+
+        return self.cx.find_one(query)
+
+    def add_sample(self, sample_object: dict) -> pymongo.results.InsertOneResult:
+        """
+        Add a sample data to mongo
+        """
+        insert_result = self.cx.insert_one(sample_object)
+        logging.debug(insert_result)
+        return insert_result
 
     @staticmethod
     def _get_mongoclient(mongo_uri: str):
+        """
+        Initializes and returns mongoclient. This will be our mocking target.
+        """
         return pymongo.MongoClient(mongo_uri)
-
-    def get_sample(self, query: dict | None):
-        if query is None:
-            query = {}
-
-        # TODO: collections
-        self.cx.find_one(query)
-
-    def add_sample(self, sample_object: dict) -> pymongo.results.InsertOneResult:
-        insert_result = self.cx.insert_one(sample_object)
-        return insert_result
-
 
 def empty_mock_mongodb():
     ...
